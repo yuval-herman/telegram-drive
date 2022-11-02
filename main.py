0353eebebebdfab26d144992e60dfa340f7dc848
yuval <nhajeho1@gmail.com>
2022-11-02 15:50:59 +0200

    added the most basic functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import logging
-from telegram import ForceReply, Update
+from telegram import ForceReply, Update, Document
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 # Enable logging
@@ -26,6 +26,23 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
 
+async def file_received(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Save file to DB"""
+    if not update.effective_user:
+        return
+    received_file = update.message.effective_attachment
+    file_name = 'unknown file'
+    if isinstance(received_file, Document):
+        file_name = f'{received_file.file_name} {received_file.mime_type} file'
+        save_file(received_file.file_id, update.effective_user.id)
+
+    await update.message.reply_text(f'Got your {file_name}!')
+
+
+def save_file(file_id: str, user_id: int):
+    print(f'file: {file_id}, user: {user_id}')
+
+
 def main() -> None:
     with open('token.txt', 'r') as token_file:
         application = Application.builder().token(token_file.read()).build()
@@ -34,6 +51,8 @@ def main() -> None:
 
     application.add_handler(MessageHandler(
         filters.TEXT & ~filters.COMMAND, echo))
+
+    application.add_handler(MessageHandler(filters.ATTACHMENT, file_received))
 
     application.run_polling()
 
